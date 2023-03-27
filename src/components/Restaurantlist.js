45e12459d105cd8517a5ce870f3400e3830ab5fa
Nijin Joy <nijinjoy1999@gmail.com>
2023-03-27 16:40:09 +0530

    api calling

--- src/components/Restaurantlist.js
+++ src/components/Restaurantlist.js
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Restaurantlist() {
+  const [restaurantlist,setRestaurantlist]= useState([])
+
+  //create a function for api call
+  const getRestaurant=async()=>{
+  //async await
+ await fetch('/restaurants.json')//asychronous call
+ .then((data)=>data.json()
+  .then((result)=>
+    setRestaurantlist(result.restaurants))
+  )
+ }
+console.log(restaurantlist);
+  
+ useEffect(()=>{
+  getRestaurant()
+ },[])
+
   return (
     <div>Restaurantlist</div>
   )
